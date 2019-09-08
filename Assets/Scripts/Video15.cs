987c4e1a94a7ff43e746b6466540d6f698238b36
Dilmer Valecillos <dilmervst@gmail.com>
2019-09-08 13:24:50 -0600

    Added video 15 and 16 for Stacks and Queues

---
+++ Assets/Scripts/Video15.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Video15 : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI stackDebug;
+
+    void Start()
+    {
+        // define your stack
+        Stack stack = new Stack();
+
+        // prep our data
+        int firstPlayerAge = 25;
+        int secondPlayerAge = 10;
+        int thirdPlayerAge = 30;
+
+        // push a new player age into the stack
+        stack.Push(firstPlayerAge);
+        stack.Push(secondPlayerAge);
+        stack.Push(thirdPlayerAge);
+
+        // show information
+        showInformationInStack(stack);
+
+        // pop player age out of the stack
+        stack.Pop();
+
+        // show information
+        showInformationInStack(stack);
+
+        // pop player age out of the stack
+        stack.Pop();
+
+        // show information
+        showInformationInStack(stack);
+
+        // pop player age out of the stack
+        stack.Pop();
+
+        // show information
+        showInformationInStack(stack);
+    }
+
+    void showInformationInStack(Stack stack)
+    {
+        stackDebug.text += "-------------------------\n\n";
+        foreach (var stackItem in stack)
+        {
+            stackDebug.text += $"Stack Item -> {stackItem} \n";   
+        }
+    }
+}
---
+++ Assets/Scripts/Video16.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Video16 : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI queueDebug;
+    
+    void Start()
+    {
+        // taxi queue 
+        Queue taxiQueue = new Queue();
+
+        // queue a new person
+        taxiQueue.Enqueue("John Doe");
+        taxiQueue.Enqueue("Jacky Chan");
+        taxiQueue.Enqueue("Michael Jordan");
+        taxiQueue.Enqueue("Steve Jobs");
+
+        // show information in queue
+        showInformationInQueue(taxiQueue);
+
+        // take a taxi
+        string personTakingTaxi = taxiQueue.Dequeue().ToString();
+        queueDebug.text += $"***Person taking a taxi: {personTakingTaxi}\n\n";
+
+        // show information in queue
+        showInformationInQueue(taxiQueue);
+
+        string secondPersonTakingTaxi = taxiQueue.Dequeue().ToString();
+        string thirdPersonTakingTaxi = taxiQueue.Dequeue().ToString();
+        queueDebug.text += $"***Person taking a taxi: {secondPersonTakingTaxi}\n\n";
+        queueDebug.text += $"***Person taking a taxi: {thirdPersonTakingTaxi}\n\n";
+
+        // show information in queue
+        showInformationInQueue(taxiQueue);
+    }
+
+    void showInformationInQueue(Queue queue)
+    {
+        queueDebug.text += "-------------------------\n\n";
+        foreach (var queueItem in queue)
+        {
+            queueDebug.text += $"Queue Item -> {queueItem} \n";   
+        }
+    }
+}
